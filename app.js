e65594d952c42b925417c1092629aad94541444e
Ruoqi Huang <huangruoqi374@gmail.com>
2020-08-30 15:28:22 -0700

    disabled checkbox

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const hwList = document.querySelector('#checkboxes');
 
 function checkOrUncheck(doc) {
 	if (doc.data().isFinished) {
-		document.getElementById(doc.data().id).checked = true;
+
+		const cb = document.getElementById(doc.data().id);
+		cb.checked = true;
+		cb.disabled = true;
+
 	}
 
 }
@@ -14,14 +18,25 @@ db.collection('Homework').get().then((snapshot)=>{
 	})
 })
 
-const submit = document.getElementById('btn');
+const submit = document.getElementById('submitButton');
 submit.addEventListener('click', element=> {
 	const cb = document.getElementsByClassName('cb');
 
 	for (var i = 0; i<cb.length;i++) {
 		const doc = db.collection('Homework').doc(cb[i].id);
+		if (cb[i].checked) {
+			cb[i].disabled = true;
+		}
 		doc.update({
 			'isFinished': cb[i].checked
 		})
+	}
+})
+const clear = document.getElementById('clearButton');
+clear.addEventListener('click', element=> {
+	const cb = document.getElementsByClassName('cb');
+	for (var i =0;i<cb.length;i++) {
+		cb[i].disabled = false;
+		cb[i].checked = false;
 	}
 })
